d14511d0560b72cd472f11f1648f82d4634284c2
ShadiK1999 <123907887+ShadiK1999@users.noreply.github.com>
2023-02-17 10:47:58 +1100

    Create operator functions and HTML buttons

---
+++ script.js
@@ -0,0 +1,26 @@
+function add(a, b){
+    added = parseInt(a + b);
+    return added;
+    // let fullSum = numbers.reduce((partialSum, a) => partialSum + a, 0);
+    // return fullSum;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    mult = parseInt(a * b);
+    return mult;
+    // let fullMult = numbers.reduce((partialMult, a) => partialMult * a, 1);
+    // return fullMult;
+}
+
+function divide(a, b){
+    divided = parseInt(a / b);
+    return divided;
+}
+
+function operate(){
+
+}
