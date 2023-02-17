3f8c2639625062e0c3e2b3c9e46d269df09f28f3
ShadiK1999 <123907887+ShadiK1999@users.noreply.github.com>
2023-02-17 16:52:38 +1100

    create calculator (forgot to commit earlier)

--- script.js
+++ script.js
@@ -1,26 +1,123 @@
-function add(a, b){
-    added = parseInt(a + b);
-    return added;
-    // let fullSum = numbers.reduce((partialSum, a) => partialSum + a, 0);
-    // return fullSum;
+const numbers  = document.querySelectorAll('.number');
+const operators  = document.querySelectorAll('.operator');
+const displayScreen = document.getElementById('display');
+const clearButton = document.getElementById('clear');
+const equalsButton = document.getElementById('equals');
+const deleteButton = document.getElementById('delete');
+
+class Calculator{
+    constructor(displayScreen){
+        this.displayScreen = displayScreen;
+    }
+
+    clear(){
+        this.currentNumber = '';
+        this.previousNumber = '';
+        this.operation = undefined;
+    }
+
+    deleteNumber(){
+        this.currentNumber = this.currentNumber.slice(0, -1);
+    }
+
+    addNumber(number){
+        if(this.currentNumber == undefined || this.currentNumber == 0) {this.currentNumber = '';}
+        if(number === '.' && this.currentNumber.includes('.')) return;
+        this.currentNumber = this.currentNumber.toString() + number.toString();
+    }
+
+    addOperator(operator){
+        if(this.currentNumber === '' || this.currentNumber == undefined) return;
+        if(this.previousNumber != '') {
+            this.operate();
+        }
+        this.operator = operator;
+        this.previousNumber = this.currentNumber;
+        this.currentNumber = '0';
+    }
+
+    updateDisplay(){
+        this.displayScreen.textContent = this.currentNumber;
+    }
+
+    operate(){
+        const prevNumber = parseFloat(this.previousNumber);
+        const currNumber = parseFloat(this.currentNumber);
+
+        if(isNaN(prevNumber) || isNaN(currNumber)) return;
+
+        switch(this.operator){
+            case '+':
+                this.add(prevNumber, currNumber);
+                break;
+            case '-':
+                this.subtract(prevNumber, currNumber);
+                break;
+            case '/':
+                this.divide(prevNumber, currNumber);
+                break;
+            case 'x':
+                this.multiply(prevNumber, currNumber);
+                break;
+        }
+
+        this.updateDisplay();
+    }
+
+    add(a, b) {
+        this.currentNumber = a + b;
+        console.log(this.currentNumber);
+    };
+    
+    subtract(a, b) {
+        this.currentNumber = a - b;
+    };
+
+    multiply(a, b) {
+        this.currentNumber = a * b;
+    };
+
+    divide(a, b) {
+        if (b == 0){
+            this.currentNumber = 'Try that again and I\'ll hit ya';
+            return;
+        } 
+
+        this.currentNumber = a / b;
+    }
+
 }
 
-function subtract(a, b){
-    return a - b;
-}
+const calculator = new Calculator(displayScreen)
 
-function multiply(a, b){
-    mult = parseInt(a * b);
-    return mult;
-    // let fullMult = numbers.reduce((partialMult, a) => partialMult * a, 1);
-    // return fullMult;
-}
+numbers.forEach(number => {
+    number.addEventListener('click', () => {
+        calculator.addNumber(number.textContent);
+        calculator.updateDisplay(); 
+    })
+})
 
-function divide(a, b){
-    divided = parseInt(a / b);
-    return divided;
-}
+operators.forEach(operator => {
+    operator.addEventListener('click', () => {
+        //console.log(operator.textContent);
+        calculator.addOperator(operator.textContent);
+        calculator.updateDisplay(); 
+    })
+})
 
-function operate(){
+clearButton.addEventListener('click', () => {
+    calculator.clear();
+    calculator.updateDisplay();
+} )
 
-}
+equalsButton.addEventListener('click', () => {
+    calculator.operate();
+    calculator.updateDisplay;
+    calculator.clear();
+})
+
+deleteButton.addEventListener('click', () => {
+    calculator.deleteNumber();
+    calculator.updateDisplay();
+})
+
